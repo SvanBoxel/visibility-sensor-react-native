96280aa3155206b0cf6c68428dcca9f71c120d2b
Sebass van Boxel <svboxel@gmail.com>
2020-06-30 22:07:26 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState, useRef } from "react";
+import { View, Dimensions } from "react-native";
+
+export interface IDemensionData {
+  rectTop: number;
+  rectBottom: number;
+  rectWidth: number;
+}
+
+export interface Props {
+  /** Function that is triggered when component enters viewport */
+  onChange(visible: boolean): any;
+  /** The component that needs to be in the viewport */
+  children: React.ReactNode
+}
+
+const InViewPort: React.FC<Props> = props => {
+  const myView: any = useRef(null);
+  const [lastValue, setLastValue] = useState<boolean>(false);
+  const [dimensions, setDimensions] = useState<IDemensionData>({
+    rectTop: 0,
+    rectBottom: 0,
+    rectWidth: 0
+  });
+
+  let interval: any = null;
+
+  useEffect(() => {
+    setLastValue(false);
+    startWatching();
+    isInViewPort();
+    return stopWatching;
+  }, [dimensions.rectTop, dimensions.rectBottom, dimensions.rectWidth]);
+
+  const startWatching = () => {
+    if (interval) {
+      return;
+    }
+
+    interval = setInterval(() => {
+      if (!myView || !myView.current) {
+        return;
+      }
+
+      myView.current.measure(async (x: number, y:number, width: number, height: number, pageX: number, pageY: number) => {
+        setDimensions({
+          rectTop: pageY,
+          rectBottom: pageY + height,
+          rectWidth: pageX + width
+        });
+      });
+    }, 1000);
+  };
+
+  const stopWatching = () => {
+    interval = clearInterval(interval);
+  };
+
+  const isInViewPort = () => {
+    const window = Dimensions.get("window");
+    const isVisible =
+      dimensions.rectBottom != 0 &&
+      dimensions.rectTop >= 0 &&
+      dimensions.rectBottom <= window.height &&
+      dimensions.rectWidth > 0 &&
+      dimensions.rectWidth <= window.width;
+
+      if (lastValue !== isVisible) {
+        setLastValue(isVisible);
+        props.onChange(isVisible);
+      }
+  };
+
+  return (
+    <View collapsable={false} ref={myView} {...props}>
+      {props.children}
+      <View />
+    </View>
+  );
+};
+
+export default InViewPort;
